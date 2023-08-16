3efac74fce6505efe4fc8c70e5905704cd10c769
Tobias Latta <tobiaslatta@jpqrfh4tgg.fritz.box>
2023-08-16 09:56:14 +0200

    database creation commit

---
+++ createProductDatabase.py
@@ -0,0 +1,60 @@
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def get_product_data(url):
+    full_url = "https://" + url  # Add the scheme and the prefix otto.de
+    try:
+        response = requests.get(full_url)
+        response.raise_for_status()  # Raise an exception for non-200 status codes
+        soup = BeautifulSoup(response.content, 'html.parser')
+        product_name = soup.find('h1', class_='pdp_variation-name').text.strip()
+        product_description = soup.find('div', class_='pdp_description__text-expander').text.strip()
+        review_link = soup.find('a', class_='cr_js_customerReviewPageLink')
+        review_url = review_link['href'] if review_link else None
+        return product_name, product_description, review_url
+    except (requests.exceptions.RequestException, AttributeError) as e:
+        print(f"Failed to fetch data for URL: {full_url}, Error: {e}")
+        return None, None, None
+
+def main():
+    # Load product info data from JSON
+    with open('product_info.json', 'r') as json_file:
+        product_info_list = json.load(json_file)
+
+    # Create SQLite database and table
+    conn = sqlite3.connect('product_database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            url TEXT,
+            brand TEXT,
+            name TEXT,
+            description TEXT,
+            review_url TEXT
+        )
+    ''')
+    conn.commit()
+
+    # Iterate through product info and fetch data
+    for product_info in product_info_list:
+        href = product_info['href']
+        brand = product_info['brand_name']
+        url = 'otto.de' + href  # Add "otto.de" prefix to URL
+
+        product_name, product_description, review_url = get_product_data(url)
+
+        if product_name:
+            cursor.execute('''
+                INSERT INTO products (url, brand, name, description, review_url)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (url, brand, product_name, product_description, review_url))
+            conn.commit()
+            print("Inserted data for:", product_name)
+
+    conn.close()
+
+if __name__ == '__main__':
+    main()
---
+++ getProductLink.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def extract_product_info(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        product_info = []
+        
+        product_elements = soup.find_all('a', class_='find_tile__productLink')
+        for product_element in product_elements:
+            product_link = product_element.get('href')
+            
+            brand_element = product_element.find_next('span', class_='find_tile__brandInName')
+            brand_class = brand_element.get('class')
+            brand_name = brand_element.get_text()
+            
+            product_info.append({
+                'href': product_link,
+                'brand_name': brand_name
+            })
+            
+        return product_info
+    else:
+        return []
+
+if __name__ == '__main__':
+    with open('product_records.json', 'r') as json_file:
+        records = json.load(json_file)
+
+    product_info_list = []
+
+    for record in records:
+        url = record['url']
+        product_info = extract_product_info(url)
+        product_info_list.extend(product_info)
+
+    with open('product_info.json', 'w') as json_file:
+        json.dump(product_info_list, json_file, indent=4)
+
+    print("Product information extracted and saved to product_info.json")
