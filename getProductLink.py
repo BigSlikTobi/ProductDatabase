68aa0dd71fcf4c34d04904cf581f817a3cfefb55
Tobias Latta <tobiaslatta@jpqrfh4tgg.fritz.box>
2023-08-18 07:31:20 +0200

    ignore missing product link

--- getProductLink.py
+++ getProductLink.py
@@ -13,14 +13,17 @@ def extract_product_info(url):
         for product_element in product_elements:
             product_link = product_element.get('href')
             
-            brand_element = product_element.find_next('span', class_='find_tile__brandInName')
-            brand_class = brand_element.get('class')
-            brand_name = brand_element.get_text()
-            
-            product_info.append({
-                'href': product_link,
-                'brand_name': brand_name
-            })
+            try:
+                brand_element = product_element.find_next('span', class_='find_tile__brandInName')
+                brand_name = brand_element.get_text()
+                
+                product_info.append({
+                    'href': product_link,
+                    'brand_name': brand_name
+                })
+            except AttributeError:
+                print("Skipping URL:", url, "- Brand element not found")
+                continue
             
         return product_info
     else:
@@ -41,3 +44,4 @@ if __name__ == '__main__':
         json.dump(product_info_list, json_file, indent=4)
 
     print("Product information extracted and saved to product_info.json")
+
